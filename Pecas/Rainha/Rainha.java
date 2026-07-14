3d8413729e20a170a94ddb881e902337325fd18a
neryy7 <pedro.nery@unesp.br>
2026-07-14 18:40:13 -0300

    Implementa validação de movimento na classe Rainha(Faltando Verificação de peças no caminho)

--- Pecas/Rainha/Rainha.java
+++ Pecas/Rainha/Rainha.java
@@ -3,6 +3,21 @@ import ../Pecas;
 
 public class Rainha implements ValidarJogadas{
 
-    boolean is_valid(String posIni, String posFinal);
+     public boolean is_valid(int x1, int y1, int x2, int y2) {
+        if(x1 == x2 && y1 == y2) {
+            System.out.println("A peça não se moveu!");
+            return false;
+        }
+
+        boolean mesmaLinha = (x1 == x2);
+        boolean mesmaColuna = (y1 == y2);
+        boolean diagonal = Math.abs(x2 - x1) == Math.abs(y2 - y1);
+        if (!(mesmaLinha || mesmaColuna || diagonal)) {
+            System.out.println("Posição inválida!");
+            return false;
+        }
+
+        return true;
+    }
 
 }
