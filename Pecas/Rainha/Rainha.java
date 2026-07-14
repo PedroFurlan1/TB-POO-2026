8ec0f5238ac1c00ba827a2146f66fb2de708fa57
neryy7 <pedro.nery@unesp.br>
2026-07-14 18:49:13 -0300

    Comentários feitos e variáveis mudadas

--- Pecas/Rainha/Rainha.java
+++ Pecas/Rainha/Rainha.java
@@ -4,15 +4,16 @@ import ../Pecas;
 public class Rainha implements ValidarJogadas{
 
      public boolean is_valid(int x1, int y1, int x2, int y2) {
-        if(x1 == x2 && y1 == y2) {
+        //Verifica se a peça se moveu
+         if(x1 == x2 && y1 == y2) {
             System.out.println("A peça não se moveu!");
             return false;
         }
-
+        //Verifica se está na mesma linha, mesma coluna ou mesma diagonal
         boolean mesmaLinha = (x1 == x2);
         boolean mesmaColuna = (y1 == y2);
-        boolean diagonal = Math.abs(x2 - x1) == Math.abs(y2 - y1);
-        if (!(mesmaLinha || mesmaColuna || diagonal)) {
+        boolean mesmaDiagonal = Math.abs(x2 - x1) == Math.abs(y2 - y1);
+        if (!(mesmaLinha || mesmaColuna || mesmaDiagonal)) {
             System.out.println("Posição inválida!");
             return false;
         }
