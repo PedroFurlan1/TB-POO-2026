1f124233726c27e66091a1abcd68f159108e5229
PedroFurlan1 <pedrofurlan2610@gmail.com>
2026-07-19 13:02:58 -0300

    cavalo feito

--- pecas/cavalo/Cavalo.java
+++ pecas/cavalo/Cavalo.java
@@ -12,6 +12,10 @@ public class Cavalo extends Pecas implements  ValidarJogadas {
 
     // Metodos
     public boolean is_valid(int x1, int y1, int x2, int y2) {
+        if(super.samPlace(x1, y1, x2, y2)) return false;
+
+        if ((Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)) == 5) return true;
+        else return false;
 
     }
 
