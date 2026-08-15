b59329f5911cc6067e74b9fa16eadf021ce74b7a
gabrieldss0 <gatchmew@gmail.com>
2026-08-15 12:18:48 -0300

    Rei e Torre com movimento validado

--- pecas/rei/Rei.java
+++ pecas/rei/Rei.java
@@ -1,22 +1,38 @@
 package pecas.rei;
 
-import pecas.validar_jogadas.ValidarJogadas;
 import pecas.Pecas;
+import pecas.validar_jogadas.ValidarJogadas;
 
 
 public class Rei extends Pecas implements  ValidarJogadas {
+    private boolean cheque;
 
     //Construtor
     public Rei(Pecas.Cores cor) {
         super(cor);
+        cheque = false;
     }
 
     // Metodos
     @Override
     public boolean is_valid(int x1, int y1, int x2, int y2) {
+        if(super.samPlace(x1, y1, x2, y2)) return false;
 
+        boolean roque = false;
+
+        if (Math.abs(x2 - x1) == 2 && y1 == y2) roque = true;
+
+        if (roque == true)
+        {
+            // Aqui colocar as coisas do roque
+        }
+
+        else if (Math.abs(x2 - x1) > 1 || Math.abs(y2 - y1) > 1 && roque == false) return false;
+        
+        return true;
     }
 
-
-
+    public boolean getCheque() {
+        return cheque;
+    }
 }
--- pecas/torre/Torre.java
+++ pecas/torre/Torre.java
@@ -1,7 +1,7 @@
 package pecas.torre;
 
-import pecas.validar_jogadas.ValidarJogadas;
 import pecas.Pecas;
+import pecas.validar_jogadas.ValidarJogadas;
 
 
 public class Torre extends Pecas implements  ValidarJogadas {
@@ -14,7 +14,11 @@ public class Torre extends Pecas implements  ValidarJogadas {
     // Metodos
     @Override
     public boolean is_valid(int x1, int y1, int x2, int y2) {
+        if(super.samPlace(x1, y1, x2, y2)) return false;
 
+        if (Math.abs(y2 - y1) >= 1 && Math.abs(x2 - x1) >= 1) return false;
+
+        return true;
     }
 
 
